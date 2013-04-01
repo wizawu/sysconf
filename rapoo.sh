b2707b7fd1bdb11afb5ea0b867f9e211b343c473
wiza <wizawu@gmail.com>
2013-04-01 12:37:05 +0800

    rapoo modified

--- rapoo.sh
+++ rapoo.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # xev
-xmodmap -e "keycode 118 = Home"
-xmodmap -e "keycode 117 = End"
-xmodmap -e "keycode 119 = Prior"
-xmodmap -e "keycode 112 = Next"
-xmodmap -e "keycode 110 = Insert"
-xmodmap -e "keycode 115 = Delete"
+xmodmap -e "keycode 119 = Home"
+xmodmap -e "keycode 110 = Delete"
+xmodmap -e "keycode  62 = End"
