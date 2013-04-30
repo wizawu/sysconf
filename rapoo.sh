a98ec6cd551d1bcf19c491c371d15ba21072b3f6
wiza <wizawu@gmail.com>
2013-04-30 22:19:01 +0800

    swap caps lock and ctrl

--- rapoo.sh
+++ rapoo.sh
@@ -2,4 +2,10 @@
 # xev
 xmodmap -e "keycode 119 = Home"
 xmodmap -e "keycode 110 = Delete"
-xmodmap -e "keycode  62 = End"
+# xmodmap -e "keycode 62 = End"
+xmodmap -e "remove Lock = Caps_Lock"
+xmodmap -e "remove Control = Control_L"
+xmodmap -e "keycode 66 = Control_L"
+xmodmap -e "keycode 37 = Caps_Lock"
+xmodmap -e "add Lock = Caps_Lock"
+xmodmap -e "add Control = Control_L"
