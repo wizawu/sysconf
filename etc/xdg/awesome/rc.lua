c118b6caa9e78c03a0646010e04c23c53abf2704
wizawu <wizawu@gmail.com>
2018-09-26 00:36:32 +0800

    Remove mykeyboardlayout

--- etc/xdg/awesome/rc.lua
+++ etc/xdg/awesome/rc.lua
@@ -237,7 +237,9 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- @wizawu --
+            -- mykeyboardlayout,
+            -- @end --
             wibox.widget.systray(),
             cpuwidget,
             cputempwidget,
