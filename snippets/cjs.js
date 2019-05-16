325dfbe4307ba023b043b21e6415020336621b29
wizawu <wizawu@gmail.com>
2019-05-16 10:31:05 +0800

    Remove cjs.js

--- snippets/cjs.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll("*").forEach(n => {
-  if (!n.innerText) return;
-  let ff = window.getComputedStyle(n).getPropertyValue("font-family");
-  if (ff && !ff.endsWith("monospace")) n.style.fontFamily = "sans";
-})
