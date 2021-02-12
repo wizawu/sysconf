21f137c770cdf3a73182b37a285c15161b4b5c77
wizawu <wizawu@gmail.com>
2021-02-12 20:09:18 +0800

    Hide .notifications_view

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -1,22 +1,27 @@
 module.exports = {
   config: {
     fontSize: 12,
-    fontFamily: '"Courier Prime Code", "CloudYuanZhunGBK", "Noto Color Emoji"',
-    fontWeightBold: 'normal',
+    fontFamily: "Courier Prime Code, CloudYuanZhunGBK, Noto Color Emoji",
+    fontWeightBold: "normal",
     lineHeight: 1.2,
     letterSpacing: -1,
-    cursorColor: 'rgba(255,255,255,0.8)',
-    cursorShape: 'BEAM',
-    cursorBlink: 'true',
-    foregroundColor: '#ddd',
-    backgroundColor: '#111',
-    borderColor: '#222',
-    padding: '2px 2px',
+    cursorColor: "rgba(255,255,255,0.8)",
+    cursorShape: "BEAM",
+    cursorBlink: "true",
+    foregroundColor: "#ddd",
+    backgroundColor: "#111",
+    borderColor: "#222",
+    padding: "2px 2px",
     copyOnSelect: false,
     webGLRenderer: false,
-    webLinksActivationKey: 'ctrl',
+    webLinksActivationKey: "ctrl",
     windowSize: [600, 400],
+    css: `
+      .notifications_view {
+        display: none;
+      }
+    `
   },
-  localPlugins: ['newcwd'],
-  plugins: ['hyperminimal'],
+  localPlugins: ["newcwd"],
+  plugins: ["hyperminimal"],
 }
