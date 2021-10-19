9fbed24d50b2bc321029e054b95ae98dc00588cd
wizawu <wizawu@gmail.com>
2021-10-20 00:01:36 +0800

    Fix output

--- roles/socks/files/train.ts
+++ roles/socks/files/train.ts
@@ -10,12 +10,12 @@ const net = new brain.NeuralNetwork()
 net.train(
   data.map(it => ({
     input: [it.err0, it.err1, it.spd0, it.spd1],
-    output: [it.prefer]
+    output: [it.prefer],
   }))
 )
 
 export function classify(site: string, args: [number, number, number, number]): number {
-  const output = net.run(args)
-  log.debug(site + " - " + output)
+  const output: number = net.run(args)[0]
+  log.debug(output.toFixed(16) + " - " + site)
   return output < 0.5 ? 0 : 1
 }
