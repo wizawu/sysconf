aa78598f78e6abcc55a8af3aaebb13819e71442f
wizawu <wizawu@gmail.com>
2021-10-23 22:27:14 +0800

    Add socks data 3

--- roles/socks/files/train.ts
+++ roles/socks/files/train.ts
@@ -2,12 +2,14 @@ import * as fs from "fs"
 import * as LoggerFactory from "log4js"
 import brain from "brain.js"
 
+const pulse: Record<string, number> = {}
 const log = LoggerFactory.getLogger("\t\b\b\b\b\b\b\b")
 log.level = "debug"
 
 const data = [
-  ...JSON.parse(fs.readFileSync("./data.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.17c9e.json", "utf-8")),
+  ...JSON.parse(fs.readFileSync("./data.1.json", "utf-8")),
+  ...JSON.parse(fs.readFileSync("./data.2.json", "utf-8")),
+  ...JSON.parse(fs.readFileSync("./data.3.json", "utf-8")),
 ]
 const net = new brain.NeuralNetwork()
 net.train(
@@ -26,28 +28,33 @@ export function classify(site: string, args: [number, number, number, number, nu
     args[4] || 0,
     args[5] || 0,
   ])[0]
-  log.debug(output.toFixed(16) + " - " + site)
-  writeData(site, args)
+  if (Math.floor(Date.now() / 1000) > (pulse[site] || 0)) {
+    pulse[site] = Math.floor(Date.now() / 1000)
+    log.debug(output.toFixed(16) + " - " + site)
+  }
   return output < 0.5 ? 0 : 1
 }
 
-const mark: Record<string, number> = {}
-data.forEach(it => (mark[it.site] = it.prefer))
-function writeData(site: string, args: [number, number, number, number, number, number]): void {
-  if (mark[site] === undefined) return
-  const file = `data.${Date.now().toString(16).substr(0, 5)}.json`
-  fs.appendFileSync(
-    file,
-    JSON.stringify({
-      site,
-      err0: args[0],
-      err1: args[1],
-      spd0: args[2],
-      spd1: args[3],
-      bw0: args[4],
-      bw1: args[5],
-      prefer: mark[site],
-    }) + ",\n"
-  )
-  delete mark[site]
+// Generate training data
+export function writeData(i: number): void {
+  const store = require("./store")
+  const mark: Record<string, number> = {}
+  data.forEach(it => (mark[it.site] = it.prefer))
+  Object.keys(mark).forEach(site => {
+    const { err0, err1 } = store.countConnErr(site)
+    const { spd0, spd1, bw0, bw1 } = store.measure(site)
+    fs.appendFileSync(
+      `data.${i}.json`,
+      JSON.stringify({
+        site,
+        err0,
+        err1,
+        spd0,
+        spd1,
+        bw0,
+        bw1,
+        prefer: mark[site],
+      }) + ",\n"
+    )
+  })
 }
