71b630722f4bc28cca4b48e163aaf46218a5e6b9
wizawu <wizawu@gmail.com>
2022-01-05 22:02:25 +0800

    Update socks model

--- roles/socks/files/train.ts
+++ roles/socks/files/train.ts
@@ -8,11 +8,7 @@ log.level = "debug"
 
 const data = [
   ...JSON.parse(fs.readFileSync("./data.0.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.4.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.5.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.6.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.7.json", "utf-8")),
-  ...JSON.parse(fs.readFileSync("./data.8.json", "utf-8")),
+  ...JSON.parse(fs.readFileSync("./data.1.json", "utf-8")),
 ]
 
 const net = new brain.NeuralNetwork()
@@ -52,21 +48,93 @@ export function writeData(i: number): void {
   const store = require("./store")
   const mark: Record<string, number> = {}
   data.forEach(it => (mark[it.site] = it.prefer))
-  Object.keys(mark).forEach(site => {
-    const { err0, err1 } = store.countConnErr(site)
-    const { spd0, spd1, bw0, bw1 } = store.measure(site)
-    fs.appendFileSync(
-      `data.${i}.json`,
-      JSON.stringify({
-        site,
-        err0,
-        err1,
-        spd0,
-        spd1,
-        bw0,
-        bw1,
-        prefer: mark[site],
-      }) + ",\n"
-    )
-  })
+  const buffer: string[] = []
+  Object.keys(mark)
+    .concat(blackList)
+    .forEach(site => {
+      const { err0, err1 } = store.countConnErr(site)
+      const { spd0, spd1, bw0, bw1 } = store.measure(site)
+      buffer.push(
+        JSON.stringify({
+          site,
+          err0,
+          err1,
+          spd0,
+          spd1,
+          bw0,
+          bw1,
+          prefer: mark[site] === undefined ? 1 : mark[site],
+        })
+      )
+    })
+  fs.appendFileSync(`data.${i}.json`, "[" + buffer.join(",\n") + "]")
 }
+
+const blackList = [
+  "360yield.com",
+  "3lift.com",
+  "4dex.io",
+  "adaptv.advertising.com",
+  "addthis.com",
+  "adform.net",
+  "adnxs.com",
+  "adsrvr.org",
+  "ads.stickyadstv.com",
+  "ads.yahoo.com",
+  "advertising.com",
+  "agkn.com",
+  "amazon-adsystem.com",
+  "a-mo.net",
+  "analytics.yahoo.com",
+  "aniview.com",
+  "baidu.com",
+  "betweendigital.com",
+  "bh.contextweb.com",
+  "bidswitch.net",
+  "bing.com",
+  "casalemedia.com",
+  "c.bing.com",
+  "clarity.ms",
+  "cm.mgid.com",
+  "contextweb.com",
+  "criteo.com",
+  "dotomi.com",
+  "doubleclick.net",
+  "endpoint1.collection.us2.sumologic.com",
+  "eus.rubiconproject.com",
+  "everesttech.net",
+  "exelator.com",
+  "facebook.com",
+  "google.co.jp",
+  "google.com",
+  "go.sonobi.com",
+  "gstatic.com",
+  "hm.baidu.com",
+  "id5-sync.com",
+  "justpremium.com",
+  "kargo.com",
+  "lijit.com",
+  "mathtag.com",
+  "mgid.com",
+  "omnitagjs.com",
+  "openx.net",
+  "op.gg",
+  "outbrain.com",
+  "prebid.a-mo.net",
+  "pubmatic.com",
+  "rfihub.com",
+  "richaudience.com",
+  "rlcdn.com",
+  "rubiconproject.com",
+  "sharethrough.com",
+  "smaato.net",
+  "smartadserver.com",
+  "tapad.com",
+  "teads.tv",
+  "tpmn.co.kr",
+  "tremorhub.com",
+  "wcs.naver.com",
+  "www.clarity.ms",
+  "www.op.gg",
+  "yahoo.com",
+]
