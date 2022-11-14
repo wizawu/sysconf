8f4be616a5efcc02bd361ff817a6cc1e87b58178
wizawu <wizawu@gmail.com>
2022-11-14 17:27:36 +0800

    Don't modprobe elan_i2c

--- bin/pointer.sh
+++ bin/pointer.sh
@@ -2,6 +2,6 @@
 
 set -x
 sudo modprobe -r elan_i2c hid_multitouch
-sudo modprobe elan_i2c hid_multitouch
+sudo modprobe hid_multitouch
 xinput set-prop 'pointer:Razer Razer DeathAdder' 'libinput Accel Speed' -0.8
 xinput set-prop 'pointer:Razer Razer DeathAdder V2' 'libinput Accel Speed' -0.4
