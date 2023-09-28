42ec17836e1e65a5184a6c9b87937058e43aa528
wizawu <wizawu@gmail.com>
2023-09-28 23:40:15 +0800

    Update phpmyadmin image

--- roles/phpmyadmin/files/config.user.inc.php
+++ roles/phpmyadmin/files/config.user.inc.php
@@ -1,4 +1,17 @@
+<?php
+declare(strict_types=1);
+$cfg['blowfish_secret'] = 'FebC3cMPORiGInlJ3pbaL8yLHQjt4s6y';
+$i = 0;
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['host'] = 'mariadb';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['AllowArbitraryServer'] = true;
 $cfg['EnableAutocompleteForTablesAndColumns'] = false;
 $cfg['LoginCookieValidity'] = 86400;
 $cfg['NavigationDisplayLogo'] = false;
 $cfg['ShowDatabasesNavigationAsTree'] = false;
+?>
